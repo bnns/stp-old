ab8ffba514da1d23cc7463ee821453dac6c73dff
bnns <omethyn@gmail.com>
2014-12-07 22:41:27 -0600

    added tabletop and timeline

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,12 @@
  * Controller of the stpApp
  */
 angular.module('stpApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope', 'tabletopService', function ($scope, tabletopService) {
   	$scope.test = 'test';
-  });
+
+  	tabletopService.query(function(data){
+  		$scope.timelineData = data;
+  		$scope.timelineKeys = _.keys(data);
+  		console.log(data);
+  	});
+  }]);
