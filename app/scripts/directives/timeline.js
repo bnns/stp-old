a6b27fdb492ba0292fe614e14b55274bb50443ab
bnns <omethyn@gmail.com>
2014-12-08 20:41:09 -0600

    added timeline points

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -8,63 +8,124 @@
  */
 angular.module('stpApp')
   .directive('timeline', ['$compile', 'd3Service', function ($compile, d3Service) {
+
     return {
 	  template: '<div class=\'stp-timeline\'></div>',
       restrict: 'EA',
       scope: {
-      	name: '=stpName',
-      	points: '=points'
+      	name: '=researchName',
+      	points: '=researchPoints'
       },
       link: function (scope, element) {
 
-      	var template = '<div id="' + scope.name + '"></div>';
-
+      	var _id = scope.name.replace(/, /g, '').replace(' and ', '');
+      	var template = '<div id="' + _id + '"></div>';
 		element.html(template).show();
-
         $compile(element.contents())(scope);
 
+        //console.log(scope.points);
+
+		var width = element.parent().width();
+		var center = width / 2,
+			leftCenter = center - 25,
+	    	rightCenter = center + 25;
+		var latestTime = _.chain(scope.points)
+			.pluck('date')
+			.map(function(d){
+				return new Date(d).getTime();
+			})
+			.max()
+			.value();
+		var startDate = moment('11/01/2014', 'MM/DD/YYYY');
+		var lineStart = 50;
+		var height = (scope.points.length > 0) ? moment(latestTime).diff(startDate, 'weeks') * 50 + lineStart : 0;
+		var endPoints = [];
+
 	    d3Service.d3().then(function(d3){
-	    	var data = [1,9,4,2,6,2,5,0,5,9,7,2].map(function(d,i) {
-	    		console.log([i,d]);
-		    	return [i, d];
-			});
 
-			var w = 300,
-			h = 100;
+	    	var svg = d3.select('#' + _id)
+	    		.append('svg')
+	    		.attr('width', width)
+	    		.attr('height', height + 50);
 
-			var x = d3.scale.linear()
-				.domain([0, data.length-1])
-				.range([0, w]);
+	    	var describePath = function(d){
+	    		var pathDescription = 'M ' + center + ' 0,',
+	    			prevDate = startDate,
+	    			prevDiff = lineStart;
 
-			var y = d3.scale.linear()
-				.domain([0, 10])
-				.range([h, 0]);
+	    		_.map(d, function(entry){
+	    			prevDiff = moment(entry.date, 'MM/DD/YYYY').diff(prevDate, 'weeks') * 50 + prevDiff;
+	    			//console.log('previous difference', prevDiff);
+	    			pathDescription += 'M ' + center + ' ' + prevDiff + ',';
+	    			if (entry.filename.length){
+	    				pathDescription += 'L ' + leftCenter + ' ' + prevDiff + ',';
+	    				endPoints.push({
+	    					x: leftCenter,
+	    					y: prevDiff,
+	    					text: entry.filename,
+	    					link: entry.filelocation
+	    				});
+	    			}
+	    			if (entry.update.length){
+		    			pathDescription += 'M ' + center + ' ' + prevDiff + ',';
+		    			pathDescription += 'L ' + rightCenter + ' ' + prevDiff + ',';
+		    			endPoints.push({
+		    				x: rightCenter,
+		    				y: prevDiff,
+		    				text: entry.update,
+		    				link: null
+		    			});
+	    			}
+	    			prevDate = moment(entry.date, 'MM/DD/YYYY');
+	    		});
 
-			var line = d3.svg.line()
-				.x(function(d) { return x(d[0]); })
-				.y(function(d) { return y(d[1]); });
+				if(d.length){
+					endPoints.push({
+						x: center,
+						y: lineStart,
+						text: 'Abstract',
+						link: null
+					},
+					{
+						x: center,
+						y: height,
+						text: 'Bibliography',
+						link: null
+					});
+				}
 
-			var svg = d3.select('.stp-timeline').append('svg')
-				.attr('w', w)
-				.attr('h', h);
+				//console.log(pathDescription);
+	    		return pathDescription;
+	    	};
 
-			// add element and transition in
-			var path = svg.append('path')
-			    .attr('class', 'line')
-			    .attr('d', line(data[0]))
-				.transition()
-			    .duration(1000)
-			    .attrTween('d', pathTween);
+	    	var line = svg.append('line')
+	    		.attr('x1', center)
+				.attr('y1', lineStart)
+				.attr('x2', center)
+				.attr('y2', height)
+				.attr('stroke-width', 2)
+				.attr('stroke', '#fff');
 
-		    function pathTween() {
-		        var interpolate = d3.scale.quantile()
-		                .domain([0,1])
-		                .range(d3.range(1, data.length + 1));
+	    	var path = svg.append('path')
+	    		.attr('d', describePath(scope.points))
+	    		.attr('stroke-width', 2)
+	    		.attr('stroke', '#fff');
 
-		        return function(t) {
-		            return line(data.slice(0, interpolate(t)));
-		        };
-		    }
+	    	var circle = svg.selectAll('circle')
+	    		.data(endPoints)
+	    		.enter()
+		    	.append('circle')
+	    		.attr('cx', function(d){ return d.x; })
+			    .attr('cy', function(d){ return d.y; })
+			    .attr('r', 10)
+			    .attr('class', 'circle');
+
+			var text = svg.data(endPoints)
+	    		.enter()
+	    		.text(function(d){ return '(' + d.x + ', ' + d.y + ')'; })
+	    		.attr('x', function(d){ return d.x })
+	    		.attr('y', function(d){ return d.y })
+	    		.attr('class', 'nodeText');
         });
       }
     };
